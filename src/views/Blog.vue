5979f050e83c1b88fc82225bb180622e32ce13bc
Henry <hgrant94@gmail.com>
2020-05-05 18:45:03 -0400

    make vue app

---
+++ src/views/Blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="blog">
+    Blog
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Blog',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    Sup
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
